b8904a33766f714ffe1acdd1ae63296528f620ce
nedjahi.mourad@gmail.com <nedjahi.mourad@gmail.com@45d33b55-eaca-56ee-ac3c-6429a45c39f0>
2012-02-06 10:57:57 +0000

    mise en place

---
+++ application/language/francais/forgotten_password_lang.php
@@ -0,0 +1,14 @@
+<?php
+$lang['mail_subject'] = '[wannagreen] mot de passe oublié';
+$lang['mail_text']=
+"Bonjour,
+
+Veuillez vous rendre a l'adresse suivante pour modifier votre mot de passe :
+
+***********MAIL_LINK***********
+
+Nous vous remercions de l'intérêt que vous portez a WannaGreen, et restons à votre entière disposition pour toute information complémentaire.
+
+Cordialement,
+L’équipe WannaGreen";
+?>
