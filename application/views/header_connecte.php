159ca8a76e97912efda272745603fb09432b7b70
alice.khammy@gmail.com <alice.khammy@gmail.com@45d33b55-eaca-56ee-ac3c-6429a45c39f0>
2012-02-08 12:49:40 +0000

    lien pour afficher la page de mon profil

--- application/views/header_connecte.php
+++ application/views/header_connecte.php
@@ -1,7 +1,7 @@
 <div id="header_connecte">
 	<ul>
 		<li><a href="<?=base_url();?>accueil">Accueil</a></li>
-		<li><a href="<?=base_url();?>utilisateur/modification_profil"><?= $this->session->userdata('prenom').' '.$this->session->userdata('nom'); ?></a></li>
+		<li><a href="<?=base_url();?>utilisateur/profil/<?= $this->session->userdata('id_utilisateur')?>"><?= $this->session->userdata('prenom').' '.$this->session->userdata('nom'); ?></a></li>
 		<li><a href="<?=base_url();?>utilisateur/deconnexion">Déconnexion</a></li>
 	</ul>
 </div>
